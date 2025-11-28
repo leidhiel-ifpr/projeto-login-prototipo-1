ce9132e4c01306133f08c2cd6d29ae1e55edcc54
leidhiel-ifpr <leidhiel.ifpr@gmail.com>
2025-11-28 17:57:05 +0000

    alterado1

--- firebase-init.js
+++ firebase-init.js
@@ -1,15 +1,13 @@
-    const firebaseConfig { 
-    apiKey: "AIzaSyB3XUYSBihLvKNzWZ7S9clGkPGE6lQ0Ykk",
-    authDomain: "projeto-login-prototipo.firebaseapp.com",
-    projectId: "projeto-login-prototipo",
-    storageBucket: "projeto-login-prototipo.firebasestorage.app",
-    messagingSenderId: "892885293201",
-    appId: "1:892885293201:web:465cc1aa3b3a8da6834730",
-    measurementId: "G-M0R89V9WT1"
-  };
-
-   
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyB3XUYSBihLvKNzWZ7S9clGkPGE6lQ0Ykk",
+  authDomain: "projeto-login-prototipo.firebaseapp.com",
+  projectId: "projeto-login-prototipo",
+  storageBucket: "projeto-login-prototipo.firebasestorage.app",
+  messagingSenderId: "892885293201",
+  appId: "1:892885293201:web:465cc1aa3b3a8da6834730",
+  measurementId: "G-M0R89V9WT1"
+};
   
-
 firebase.initializeApp(firebaseConfig); 
   
